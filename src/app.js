788999555506fdb7174afa3d67b90cee582235ee
GianQuse <gianquse@gmail.com>
2026-03-04 11:35:33 -0300

    cambio conexión mongo

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import { engine } from 'express-handlebars';
+import dotenv from "dotenv";
+dotenv.config();
+import { connectDB } from "./config/dbConnection.js";
 
 import productsRouter from './routes/productsRouter.js';
 import cartsRouter from './routes/cartsRoutes.js';
 import viewsRouter from "./routes/viewsRouter.js";
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 //Middlewares
 app.use(express.json());
@@ -23,12 +25,14 @@ app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 app.use("/", viewsRouter);
 
-//Mongo 
-mongoose.connect('mongodb://localhost:27017/ecommerce')
-    .then(() => console.log('Mongo Conectado'))
-    .catch(err => console.log(err));
-
-app.listen(PORT, () => {
-    console.log(`Servidor escuchando http://localhost:${PORT}`);
-});
+// Mongo + Server
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Servidor escuchando http://localhost:${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error("Error al iniciar la aplicación:", error);
+    });
 
